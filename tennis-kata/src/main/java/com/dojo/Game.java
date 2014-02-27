d44e84b0a4ee58f6cc0e317b1fa6ccaff1f176fe
laurent leseigneur <laurent.leseigneur@bonitasoft.com>
2014-02-27 13:42:47 +0100

    refactor: remove useless getPlayerXScore methods

--- tennis-kata/src/main/java/com/dojo/Game.java
+++ tennis-kata/src/main/java/com/dojo/Game.java
@@ -1,22 +1,11 @@
 package com.dojo;
 
-import java.math.BigDecimal;
 
 
 public class Game {
 
 	private Player player1 = new Player();
 	private Player player2 = new Player();
-
-	public Point getPlayer1Score() {
-		// TODO Auto-generated method stub
-		return Point.ZERO;
-	}
-
-	public Point getPlayer2Score() {
-		// TODO Auto-generated method stub
-		return Point.ZERO;
-	}
 
 	public Player getPlayer1() {
 		// TODO Auto-generated method stub
@@ -25,7 +14,7 @@ public class Game {
 
 	public Player getPlayer2() {
 		// TODO Auto-generated method stub
-		return player2 = new Player();
+		return player2;
 	}
 
 }
