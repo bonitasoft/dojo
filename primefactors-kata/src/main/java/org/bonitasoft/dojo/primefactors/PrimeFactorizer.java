f2d75c61cfac42677c284c0a34932b7264f32676
Colin PUY <colin.puy@bonitasoft.com>
2014-03-11 13:25:00 +0100

    add prime factors kata

---
+++ primefactors-kata/src/main/java/org/bonitasoft/dojo/primefactors/PrimeFactorizer.java
@@ -0,0 +1,10 @@
+package org.bonitasoft.dojo.primefactors;
+
+import java.util.List;
+
+public class PrimeFactorizer {
+
+	public List<Integer> factorize(int number) {
+		throw new RuntimeException("Not implemented yet");
+	}
+}
---
+++ primefactors-kata/src/test/java/org/bonitasoft/dojo/primefactors/PrimeFactorizerTest.java
@@ -0,0 +1,6 @@
+package org.bonitasoft.dojo.primefactors;
+
+
+public class PrimeFactorizerTest {
+
+}
