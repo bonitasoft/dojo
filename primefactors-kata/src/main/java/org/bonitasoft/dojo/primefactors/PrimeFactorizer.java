114c7ee602f0d2484cf3a18975ddd218229a1dc7
Emmanuel Duchastenier <emmanuel.duchastenier@bonitasoft.com>
2014-03-13 13:00:24 +0100

    works for 0, 1, 2, 3, 4.

--- primefactors-kata/src/main/java/org/bonitasoft/dojo/primefactors/PrimeFactorizer.java
+++ primefactors-kata/src/main/java/org/bonitasoft/dojo/primefactors/PrimeFactorizer.java
@@ -1,10 +1,18 @@
 package org.bonitasoft.dojo.primefactors;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 public class PrimeFactorizer {
 
-	public List<Integer> factorize(int number) {
-		throw new RuntimeException("Not implemented yet");
-	}
+    public List<Integer> factorize(final int number) {
+        if (number < 2) {
+            throw new IllegalArgumentException("0 and 1 are not allowed");
+        }
+        if (number == 4) {
+            return Arrays.asList(2, 2);
+        }
+        return Collections.singletonList(number);
+    }
 }
--- primefactors-kata/src/test/java/org/bonitasoft/dojo/primefactors/PrimeFactorizerTest.java
+++ primefactors-kata/src/test/java/org/bonitasoft/dojo/primefactors/PrimeFactorizerTest.java
@@ -1,6 +1,32 @@
 package org.bonitasoft.dojo.primefactors;
 
+import static org.assertj.core.api.Assertions.assertThat;
+
+import java.util.List;
+
+import org.junit.Test;
 
 public class PrimeFactorizerTest {
 
+    @Test(expected = IllegalArgumentException.class)
+    public void factorize_should_throw_exception_if_0() throws Exception {
+        new PrimeFactorizer().factorize(0);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void factorize_should_throw_exception_if_1() throws Exception {
+        new PrimeFactorizer().factorize(1);
+    }
+
+    @Test
+    public void factorize_2_should_return_2() throws Exception {
+        List<Integer> factorize = new PrimeFactorizer().factorize(2);
+        assertThat(factorize).containsExactly(2);
+    }
+
+    @Test
+    public void factorize_4_should_return_2_2() throws Exception {
+        List<Integer> factorize = new PrimeFactorizer().factorize(4);
+        assertThat(factorize).containsExactly(2, 2);
+    }
 }
