29744f47b85b6b830c90c0968a04f9239abe4fe4
Emmanuel Duchastenier <emmanuel.duchastenier@bonitasoft.com>
2014-03-13 13:43:27 +0100

    Valid for all tested numbers up to 82.

--- primefactors-kata/src/main/java/org/bonitasoft/dojo/primefactors/PrimeFactorizer.java
+++ primefactors-kata/src/main/java/org/bonitasoft/dojo/primefactors/PrimeFactorizer.java
@@ -1,18 +1,34 @@
 package org.bonitasoft.dojo.primefactors;
 
-import java.util.Arrays;
-import java.util.Collections;
+import java.util.ArrayList;
 import java.util.List;
 
 public class PrimeFactorizer {
+
+    private static final int FIRST_LEGAL_VALUE = 2;
 
     public List<Integer> factorize(final int number) {
         if (number < 2) {
             throw new IllegalArgumentException("0 and 1 are not allowed");
         }
-        if (number == 4) {
-            return Arrays.asList(2, 2);
+
+        int rest = number;
+        List<Integer> result = new ArrayList<Integer>();
+        for (int divisor = FIRST_LEGAL_VALUE; divisor <= rest; divisor++) {
+            rest = addIfDivisible(result, rest, divisor);
         }
-        return Collections.singletonList(number);
+        return result;
+    }
+
+    protected int addIfDivisible(final List<Integer> result, int number, final int divisor) {
+        while (isDivisible(number, divisor)) {
+            result.add(divisor);
+            number /= divisor;
+        }
+        return number;
+    }
+
+    private boolean isDivisible(final int number, final int factor) {
+        return number % factor == 0;
     }
 }
--- primefactors-kata/src/test/java/org/bonitasoft/dojo/primefactors/PrimeFactorizerTest.java
+++ primefactors-kata/src/test/java/org/bonitasoft/dojo/primefactors/PrimeFactorizerTest.java
@@ -27,6 +27,30 @@ public class PrimeFactorizerTest {
     @Test
     public void factorize_4_should_return_2_2() throws Exception {
         List<Integer> factorize = new PrimeFactorizer().factorize(4);
-        assertThat(factorize).containsExactly(2, 2);
+        assertThat(factorize).containsOnly(2, 2);
+    }
+
+    @Test
+    public void factorize_6_should_return_2_3() throws Exception {
+        List<Integer> factorize = new PrimeFactorizer().factorize(6);
+        assertThat(factorize).containsOnly(2, 3);
+    }
+
+    @Test
+    public void factorize_15_should_return_3_5() throws Exception {
+        List<Integer> factorize = new PrimeFactorizer().factorize(15);
+        assertThat(factorize).containsOnly(3, 5);
+    }
+
+    @Test
+    public void factorize_13_should_return_13() throws Exception {
+        List<Integer> factorize = new PrimeFactorizer().factorize(13);
+        assertThat(factorize).containsOnly(13);
+    }
+
+    @Test
+    public void factorize_82_should_return_2_41() throws Exception {
+        List<Integer> factorize = new PrimeFactorizer().factorize(82);
+        assertThat(factorize).containsOnly(2, 41);
     }
 }
