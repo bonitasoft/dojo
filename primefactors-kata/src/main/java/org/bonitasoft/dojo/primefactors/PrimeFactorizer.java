80cd5afa63200e1b9a1503f9168dcdae36a6174a
Colin PUY <colin.puy@bonitasoft.com>
2014-03-27 11:44:27 +0100

    primeFacotrs - add implemenatation in master branch

--- primefactors-kata/src/main/java/org/bonitasoft/dojo/primefactors/PrimeFactorizer.java
+++ primefactors-kata/src/main/java/org/bonitasoft/dojo/primefactors/PrimeFactorizer.java
@@ -1,10 +1,28 @@
 package org.bonitasoft.dojo.primefactors;
 
+import static java.util.Collections.emptyList;
+
+import java.util.ArrayList;
 import java.util.List;
 
 public class PrimeFactorizer {
 
-	public List<Integer> factorize(int number) {
-		throw new RuntimeException("Not implemented yet");
+	public List<Integer> factorize(final int number) {
+		Factorizable factorizable = new Factorizable(number);
+		if (factorizable.canBeFactorized())
+			return factorize(factorizable);
+		else
+			return emptyList();
+	}
+
+	private List<Integer> factorize(Factorizable factorizable) {
+		List<Integer> factors = new ArrayList<Integer>();
+		for (int factor = 2; factorizable.greaterOrEqualsThan(factor); factor++) {
+			while (factorizable.isFactorizableBy(factor)) {
+				factors.add(factor);
+				factorizable.factorize(factor);
+			}
+		}
+		return factors;
 	}
 }
