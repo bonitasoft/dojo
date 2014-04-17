8d39b9ccd1dab5918583303fcd83912f6cfeb29b
Colin PUY <colin.puy@bonitasoft.com>
2014-04-17 09:23:42 +0200

    [codegen] add cucumber runner class, change package

---
+++ code-generator-kata/src/test/features/org/bonitasoft/dojo/codegen/RunCukesTest.java
@@ -0,0 +1,11 @@
+package org.bonitasoft.dojo.codegen;
+
+import cucumber.api.CucumberOptions;
+import cucumber.api.junit.Cucumber;
+
+import org.junit.runner.RunWith;
+
+@RunWith(Cucumber.class)
+@CucumberOptions(format = { "html:target/cucumber-html-report" })
+public class RunCukesTest {
+}
