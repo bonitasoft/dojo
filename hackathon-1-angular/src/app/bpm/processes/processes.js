0ceb62b771ff3a8b696cf4f09c5b9a85a3467403
julien-reboul <julien.reboul@bonitasoft.com>
2014-05-12 22:13:51 +0200

    add form iframe directly in start for

--- hackathon-1-angular/src/app/bpm/processes/processes.js
+++ hackathon-1-angular/src/app/bpm/processes/processes.js
@@ -14,16 +14,33 @@ angular.module('processes', ['resources.processes', 'resources.users'])
         };
     }]).controller('StartProcessCtrl', ['$scope', 'Processes', 'Users', '$location', '$http', '$route',
         function ($scope, Processes, Users, $location, $http, $route) {
-            Processes
-                .getById($route.current.params.processId, processCallback);
+            Processes.getById($route.current.params.processId, processCallback);
             Users.getById($route.current.params.userId, userCallback)
 
             function userCallback(data) {
                 $scope.user = data;
+                if($scope.process){
+                    $scope.src= $scope.renderSrc($scope);
+                }
             };
 
             function processCallback(data) {
                 $scope.process = data;
+                if($scope.user){
+                    $scope.src= $scope.renderSrc($scope);
+                }
             };
+            $scope.renderSrc = function(_scope){
+                return '/bonita/portal/homepage?ui=form&locale=en#' +
+                    'form='+_scope.process.name+'--'+_scope.process.version+'$entry&' +
+                    'process='+_scope.process.id+'&autoInstantiate=false&mode=form&' +
+                    'userId='+_scope.user.id;
+            }
 
-        }]);
+        }]).directive('startProcess', function() {
+        return {
+            scope : false,
+            restrict : 'E',
+            template: '<iframe id="bonitaframe" ng-src="{{src}}" class="whole-page"></iframe>'
+        }
+    });
