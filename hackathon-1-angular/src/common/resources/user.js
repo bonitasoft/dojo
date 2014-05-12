c6f4fc64dc0518698e9dd4233a97c2c48ed32841
Charles Souillard <charles.souillard@bonitasoft.com>
2014-05-12 16:59:16 +0200

    add getTeamMember function to Users resource

--- hackathon-1-angular/src/common/resources/user.js
+++ hackathon-1-angular/src/common/resources/user.js
@@ -3,13 +3,21 @@
 /* Module declaration for User resource */
 
 angular.module('resources.users', ['bonitaResource']);
-angular.module('resources.users').factory('Users', ['bonitaResource', function (bonitaResource) {
+angular.module('resources.users')
 
-  var userResource = bonitaResource('users','../bonita/API/identity/user/');
+    .factory('Users', ['bonitaResource', function (bonitaResource) {
+
+        var userResource = bonitaResource('users','../bonita/API/identity/user/');
   
-  userResource.prototype.getFullName = function () {
-    return this.lastname + " " + this.firstname + " (" + this.userName + ")";
-  };
+        userResource.prototype.getFullName = function () {
+            return this.lastname + " " + this.firstname + " (" + this.userName + ")";
+        };
 
-  return userResource;
-}]);
+        userResource.getTeamMembers = function (managerUserId, cb, errorcb) {
+            return userResource.query({p:0,c:10000,o:'lastname ASC',f:'manager_id=' + managerUserId}, cb, errorcb);
+        };
+
+        return userResource;
+    }])
+
+;
