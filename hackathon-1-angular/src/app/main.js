60e0e7e87d7a2af163fd5399b74e759d5148d4cb
abirembaut <anthony.birembaut@bonitasoft.com>
2014-05-12 17:36:49 +0200

    add TM menu

--- hackathon-1-angular/src/app/main.js
+++ hackathon-1-angular/src/app/main.js
@@ -4,14 +4,14 @@
 
 angular.module('BonitaBPM6Portal', ['ngRoute','admin', 'BonitaBPM6Portal.version', 'BonitaBPM6Portal.appVersion', 'login', 'tm'])
 
-    .value('loggedUser', {username:'', id: ''})
+    .value('loggedUser', {username:'', id: '', isAdmin:false})
 
     .config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
         $routeProvider.when("/login", {templateUrl: 'app/login/login-tpl.html'});
         $routeProvider.otherwise({redirectTo:'/admin/users'});
     }])
 
-    .controller('MainCtrl', ['$scope', '$http', 'loggedUser', function($scope, $http, loggedUser){
+    .controller('MainCtrl', ['$scope', '$http', '$location', 'loggedUser', function($scope, $http, $location, loggedUser){
         $scope.loggedUser = loggedUser;
         $http({
             method: 'GET',
@@ -19,6 +19,10 @@ angular.module('BonitaBPM6Portal', ['ngRoute','admin', 'BonitaBPM6Portal.version
         }).success(function(data){
             $scope.loggedUser.username = data.user_name;
             $scope.loggedUser.id = data.user_id;
+            $scope.location=$location;
+            $scope.$watch('location.path()', function(path) {
+                $scope.loggedUser.isAdmin = path.startsWith('/admin');
+            });
         });
     }]);
 
