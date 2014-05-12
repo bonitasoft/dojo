3308b686a586be053045d51904010978388cf39b
Colin PUY <colin.puy@bonitasoft.com>
2014-05-12 13:41:52 +0200

    remove manager from user edit view

--- hackathon-1-angular/src/common/resources/user.js
+++ hackathon-1-angular/src/common/resources/user.js
@@ -6,7 +6,7 @@ angular.module('resources.users', ['bonitaResource']);
 angular.module('resources.users')
     .factory('Users', ['bonitaResource', function (bonitaResource) {
 
-  var userResource = bonitaResource('users','../bonita/API/identity/user/', { d: 'manager_id' });
+  var userResource = bonitaResource('users','../bonita/API/identity/user/');
   
   userResource.prototype.getFullName = function () {
     return this.lastname + " " + this.firstname + " (" + this.userName + ")";
