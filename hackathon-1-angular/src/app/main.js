4227c8661a8431cad1ff6ce9913a94d4765ee78b
Charles Souillard <charles.souillard@bonitasoft.com>
2014-05-12 12:46:13 +0200

    display profiles instead of pages when no profile is active

--- hackathon-1-angular/src/app/main.js
+++ hackathon-1-angular/src/app/main.js
@@ -5,18 +5,22 @@
 angular.module('BonitaBPM6Portal', ['ngRoute','admin','BonitaBPM6Portal.version', 'BonitaBPM6Portal.appVersion', 'login'])
 
     .value('loggedUser', {username:''})
+    .value('activeProfile', '')
+
 
     .config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
         $routeProvider.otherwise({template:"<div>404 :-)</div>"});
     }])
 
-    .controller('MainCtrl', ['$scope', '$http', 'loggedUser', function($scope, $http, loggedUser){
+    .controller('MainCtrl', ['$scope', '$http', 'loggedUser', 'activeProfile', function($scope, $http, loggedUser, activeProfile){
         $scope.loggedUser = loggedUser;
+        $scope.activeProfile = activeProfile;
         $http({
             method: 'GET',
             url: 'bonita/API/system/session/unusedid'
         }).success(function(data){
             $scope.loggedUser.username = data.user_name;
+            $scope.activeProfile = '';
         });
     }]);
 
