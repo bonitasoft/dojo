44a07ceb1b95c197a296800a65dc02e372a7bf87
lombafab <fabio.lombardi@bonitasoft.com>
2014-05-12 17:46:08 +0200

    list of manager of

--- hackathon-1-angular/src/app/tm/users/tm-users.js
+++ hackathon-1-angular/src/app/tm/users/tm-users.js
@@ -1,100 +1,111 @@
-angular.module('tm-users', ['services.crud', 'directives.crud', 'directives.gravatar','resources.users', 'tm-users-edit-uniqueUsername'])
-.config(['crudRouteProvider', function (crudRouteProvider) {
+angular.module('tm-users', ['services.crud', 'directives.crud', 'directives.gravatar', 'resources.users', 'tm-users-edit-uniqueUsername'])
+    .config(['crudRouteProvider', function (crudRouteProvider) {
 
-  crudRouteProvider.routesFor('Users', 'tm')
-    .whenList({
-      users: ['Users', function(Users) { return Users.all(); }]
-    })
-    .whenNew({
-      user: ['Users', function(Users) { return new Users(); }]
-    })
-    .whenEdit({
-      user:['$route', 'Users', function ($route, Users) {
-        return Users.getById($route.current.params.itemId);
-      }]
-    });
-}])
-.controller('tmUsersListCtrl', ['$scope', 'crudListMethods', '$filter', 'users', 'loggedUser', function ($scope, crudListMethods, $filter, users, loggedUser) {
-  $scope.users = users;
+        crudRouteProvider.routesFor('Users', 'tm')
+            .whenList({
+                users: ['Users', function (Users) {
+                    return Users.all();
+                }]
+            })
+            .whenNew({
+                user: ['Users', function (Users) {
+                    return new Users();
+                }]
+            })
+            .whenEdit({
+                user: ['$route', 'Users', function ($route, Users) {
+                    return Users.getById($route.current.params.itemId);
+                }]
+            });
+    }])
+    .controller('tmUsersListCtrl', ['$scope', 'crudListMethods', '$filter', 'users', 'loggedUser', function ($scope, crudListMethods, $filter, users, loggedUser) {
+        $scope.users = users;
 
-  angular.extend($scope, crudListMethods('/tm/users'));
+        angular.extend($scope, crudListMethods('/tm/users'));
 
-  $scope.displayUsersPossibleValues = [
-    {name:'MyTeam', filterItems:function(user) {
-          return user.manager_id == loggedUser.id;
-    }},
-	{name:'Active', filterItems:function(user) {
-		return user.manager_id == loggedUser.id && user.enabled;
-	}},
-	{name:'Inactive', filterItems:function(user) {
-		return user.manager_id == loggedUser.id && !user.enabled;
-	}}
-  ];
+        $scope.displayUsersPossibleValues = [
+            {name: 'MyTeam', filterItems: function (user) {
+                return user.manager_id == loggedUser.id;
+            }},
+            {name: 'Active', filterItems: function (user) {
+                return user.manager_id == loggedUser.id && user.enabled;
+            }},
+            {name: 'Inactive', filterItems: function (user) {
+                return user.manager_id == loggedUser.id && !user.enabled;
+            }}
+        ];
 
-  $scope.displayUsers = $scope.displayUsersPossibleValues[0];
-  
-  
-  $scope.isUserVisible = function (item) {
-	return $scope.displayUsers.filterItems(item);
-  };
-  
-  $scope.disable = function(user, $index, $event) {
-	  
-	  $event.stopPropagation();
-	  
-	  user.$disable(function() {
-	      $scope.users[$index].enabled = false;
-	    }, function() {
-	    	console.log("User cannot be disabled: " + user.$id());
-	    });
-    };
-    
-    $scope.enable = function(user, $index, $event) {
-  	  
-  	  $event.stopPropagation();
-  	  
-  	  user.$enable(function() {
-  	      user.enabled = true;
-  	    }, function() {
-  	    	console.log("User cannot be enabled: " + user.$id());
-  	    });
-      };
-  
-  
-  $scope.remove = function(user, $index, $event) {
-    // Don't let the click bubble up to the ng-click on the enclosing div, which will try to trigger
-    // an edit of this item.
-    $event.stopPropagation();
+        $scope.displayUsers = $scope.displayUsersPossibleValues[0];
 
-    // Remove this user
-    user.$remove(function() {
-      // It is gone from the DB so we can remove it from the local list too
-      $scope.users.splice($index,1);
-    }, function() {
-    	console.log("User cannot be removed: " + user.$id());
-    });
-  };
-}])
-.controller('tmUsersEditCtrl', ['$scope', '$location', '$filter', 'user', function ($scope, $location, $filter, user) {
 
-  $scope.user = user;
-  $scope.password = user.password;
+        $scope.isUserVisible = function (item) {
+            return $scope.displayUsers.filterItems(item);
+        };
 
-	$scope.cancelEdit = function() {
-		$location.path('/tm/users');
-	};
-  
-  $scope.onSave = function (user) {
-    $location.path('/tm/users');
-  };
+        $scope.disable = function (user, $index, $event) {
 
-  $scope.onError = function() {
-	  console.log("Unable to save user!");
-  };
+            $event.stopPropagation();
 
-  $scope.onRemove = function(user) {
-    $location.path('/tm/users');
-  };
+            user.$disable(function () {
+                $scope.users[$index].enabled = false;
+            }, function () {
+                console.log("User cannot be disabled: " + user.$id());
+            });
+        };
 
-}])
+        $scope.enable = function (user, $index, $event) {
+
+            $event.stopPropagation();
+
+            user.$enable(function () {
+                user.enabled = true;
+            }, function () {
+                console.log("User cannot be enabled: " + user.$id());
+            });
+        };
+
+
+        $scope.remove = function (user, $index, $event) {
+            // Don't let the click bubble up to the ng-click on the enclosing div, which will try to trigger
+            // an edit of this item.
+            $event.stopPropagation();
+
+            // Remove this user
+            user.$remove(function () {
+                // It is gone from the DB so we can remove it from the local list too
+                $scope.users.splice($index, 1);
+            }, function () {
+                console.log("User cannot be removed: " + user.$id());
+            });
+        };
+    }])
+    .controller('tmUsersEditCtrl', ['$scope', '$location', '$filter', 'user', '$http', function ($scope, $location, $filter, user, $http) {
+
+        $scope.user = user;
+        $scope.password = user.password;
+
+        $http({method: 'GET',
+            url: '../bonita/API/identity/user/?f=manager_id=' + user.id
+        })
+        .success(function (data, status, headers, config) {
+             $scope.managedUsers = data;
+        })
+
+        $scope.cancelEdit = function () {
+            $location.path('/tm/users');
+        };
+
+        $scope.onSave = function (user) {
+            $location.path('/tm/users');
+        };
+
+        $scope.onError = function () {
+            console.log("Unable to save user!");
+        };
+
+        $scope.onRemove = function (user) {
+            $location.path('/tm/users');
+        };
+
+    }])
 ;
