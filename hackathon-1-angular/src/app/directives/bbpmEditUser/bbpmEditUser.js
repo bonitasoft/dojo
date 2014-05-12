4e14ebce98f140a205da3c294f819e50fff37b49
Vincent <vincent.elcrin@gmail.com>
2014-05-12 18:36:06 +0200

    Add form validator director for password repeat.

--- hackathon-1-angular/src/app/directives/bbpmEditUser/bbpmEditUser.js
+++ hackathon-1-angular/src/app/directives/bbpmEditUser/bbpmEditUser.js
@@ -19,5 +19,22 @@ angular.module('bonitasoft.bbpmEditUser', [])
             scope: {user: '='},
             templateUrl: 'app/directives/bbpmEditUser/bbpmEditUser-tpl.html'
         };
+    }).directive('repeat', function() {
+        return {
+            require: 'ngModel',
+            link: function(scope, elm, attrs, ctrl) {
+                ctrl.$parsers.unshift(function(value) {
+                    console.log(value);
+                    console.log(scope);
+                    if (scope.repeat === value) {
+                        ctrl.$setValidity('repeat', true);
+                        return value;
+                    } else {
+                        ctrl.$setValidity('repeat', false);
+                        return undefined;
+                    }
+                });
+            }
+        };
     });
 
