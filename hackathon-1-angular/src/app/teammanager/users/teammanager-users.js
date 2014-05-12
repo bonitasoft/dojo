34ef5bc6735b1c3cc1bbac1b222e617546a5924f
ntith <nicolas.tith@bonitasoft.com>
2014-05-12 10:56:01 +0200

    init team manager module

---
+++ hackathon-1-angular/src/app/teammanager/users/teammanager-users.js
@@ -0,0 +1,16 @@
+angular.module('admin-users', ['services.crud', 'directives.crud', 'directives.gravatar','resources.users', 'admin-users-edit-uniqueUsername'])
+    .config(['crudRouteProvider', function (crudRouteProvider) {
+
+        crudRouteProvider.routesFor('Users', 'teammanager')
+            .whenList({
+                users: ['Users', function(Users) { return Users.all(); }]
+            })
+            /*.whenNew({
+                user: ['Users', function(Users) { return new Users(); }]
+            })*/
+            .whenEdit({
+                user:['$route', 'Users', function ($route, Users) {
+                    return Users.getById($route.current.params.itemId);
+                }]
+            });
+    }]);
