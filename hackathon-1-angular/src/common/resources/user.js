f83e6ab750015125002c14c719577b0cc29f6837
Colin PUY <colin.puy@bonitasoft.com>
2014-05-12 10:49:02 +0200

    add manager in user list

--- hackathon-1-angular/src/common/resources/user.js
+++ hackathon-1-angular/src/common/resources/user.js
@@ -3,9 +3,10 @@
 /* Module declaration for User resource */
 
 angular.module('resources.users', ['bonitaResource']);
-angular.module('resources.users').factory('Users', ['bonitaResource', function (bonitaResource) {
+angular.module('resources.users')
+    .factory('Users', ['bonitaResource', function (bonitaResource) {
 
-  var userResource = bonitaResource('users','../bonita/API/identity/user/');
+  var userResource = bonitaResource('users','../bonita/API/identity/user/', { d: 'manager_id' });
   
   userResource.prototype.getFullName = function () {
     return this.lastname + " " + this.firstname + " (" + this.userName + ")";
