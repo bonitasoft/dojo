aa144f6c166c0d1d11af7dfa7582d5ed58d5298e
Vincent <vincent.elcrin@gmail.com>
2014-05-13 09:40:59 +0200

    Complete karma configuration.

--- hackathon-1-angular/test/bbpmManagerUserList.spec.js
+++ hackathon-1-angular/test/bbpmManagerUserList.spec.js
@@ -1,19 +1,19 @@
 describe('DirectiveSeed', function () {
 
-    var $compile, $controller, $rootScope;
+    var $compile, $controller, $scope;
 
-    beforeEach(module('bonitasoft.bbpmManagerUserList', 'bbpmManagerUserList-tpl.hmtl'));
+    beforeEach(module('bonitasoft.bbpmManagerUserList', 'app/directives/bbpmManagerUserList/bbpmManagerUserList-tpl.html'));
 
     beforeEach(inject(function (_$compile_, _$controller_, _$rootScope_) {
         $compile = _$compile_;
         $controller = _$controller_;
-        $rootScope = _$rootScope_;
+        $scope = _$rootScope_.$new();
     }));
 
     describe('directive', function () {
         it('should contains welcome message', function () {
-            var compiled = $compile('<bbpm-manager-user-list></bbpm-manager-user-list>')($rootScope);
-            $rootScope.$apply();
+            var compiled = $compile('<bbpm-manager-user-list></bbpm-manager-user-list>')($scope);
+            $scope.$digest();
         });
     });
 
