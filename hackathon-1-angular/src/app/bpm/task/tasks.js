2ef4e2ef581ce63cfc6af5f2e1291aaab89ba0fc
Colin PUY <colin.puy@bonitasoft.com>
2014-05-13 10:17:05 +0200

    see tasks of my team member

--- hackathon-1-angular/src/app/bpm/task/tasks.js
+++ hackathon-1-angular/src/app/bpm/task/tasks.js
@@ -1,6 +1,12 @@
 angular.module('tasks', ['resources.task'])
-    .controller('TaskCtrl', function($scope, Tasks, $route) {
-        Tasks.query({f: ['state=ready', "user_id="+$route.current.params.userId]});
+    .controller('TaskCtrl', function($scope, Tasks, $route, Users) {
+        Tasks.query({f: ['state=ready', "user_id="+$route.current.params.userId]}, callback);
+
+        function userCallback(data) {
+            $scope.user =  data;
+        }
+
+        Users.getById($route.current.params.userId, userCallback);
 
         function callback(data) {
             $scope.tasks = data;
