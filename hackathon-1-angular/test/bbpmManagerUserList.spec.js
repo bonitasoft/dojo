3dece4be39b764207ac321a43fd96bae01688fdb
julienmege <julien.mege@bonitasoft.com>
2014-05-13 13:30:13 +0200

    test on directive

--- hackathon-1-angular/test/bbpmManagerUserList.spec.js
+++ hackathon-1-angular/test/bbpmManagerUserList.spec.js
@@ -1,4 +1,4 @@
-describe('DirectiveSeed', function () {
+describe('BPM Manager User list', function () {
 
     var $compile, $controller, $scope, $httpBackend;
 
@@ -12,10 +12,20 @@ describe('DirectiveSeed', function () {
     }));
 
     describe('directive', function () {
-        it('should contains welcome message', function () {
-            $httpBackend.whenGET('bonita/API/identity/user?c=10&f=manager_id%3Dundefined&o=lastname+ASC&p=0').respond("");
-            var compiled = $compile('<bbpm-manager-user-list></bbpm-manager-user-list>')($scope);
+        it('should call the first le sub user list related to the given Id ', function () {
+            $httpBackend.whenGET('bonita/API/identity/user?c=10&f=manager_id%3D1&o=lastname+ASC&p=0').respond(
+                [{  "id":"2",
+                    "manager_id":"1",
+                    "userName":"daniela.angelo",
+                    "lastname":"Angelo",
+                    "firstname":"Daniela"
+                }]);
+            $httpBackend.whenGET('bonita/API/identity/user?c=10&f=manager_id%3D2&o=lastname+ASC&p=0').respond({});
+            var compiled = $compile('<bbpm-manager-user-list user-id="1"></bbpm-manager-user-list>')($scope);
             $scope.$digest();
+            $httpBackend.flush();
+
+            expect(compiled.find('button').length).toBe(1);
         });
     });
 
