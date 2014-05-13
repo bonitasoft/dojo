1a9b46f32bb308b2518cb0ac112e5d8aab937105
Colin PUY <colin.puy@bonitasoft.com>
2014-05-13 11:25:16 +0200

    change route for processes

--- hackathon-1-angular/src/app/route.js
+++ hackathon-1-angular/src/app/route.js
@@ -5,9 +5,11 @@ angular.module('routes', [])
     .config(['$routeProvider', '$locationProvider', function ($routeProvider, $locationProvider) {
 
         $routeProvider.when("/teammanager/users", {templateUrl: 'app/teammanager/users.tpl.html', controller: 'listUsersCtrl'});
-        $routeProvider.when("/bpm/processes/:id", {templateUrl: 'app/bpm/processes/processes-tpl.html', controller: 'ProcessesCtrl'});
-        $routeProvider.when("/bpm/process/:processId/:userId", {templateUrl: 'app/bpm/processes/start-process-tpl.html', controller: 'StartProcessCtrl'});
         $routeProvider.when("/teammanager/users/:id", {templateUrl: 'app/teammanager/user.tpl.html', controller: 'editUserCtrl'});
+        $routeProvider.when("/teammanager/users/:id/processes", {templateUrl: 'app/bpm/processes/processes-tpl.html', controller: 'ProcessesCtrl'});
+
+        $routeProvider.when("/bpm/process/:processId/:userId", {templateUrl: 'app/bpm/processes/start-process-tpl.html', controller: 'StartProcessCtrl'});
+
 
         $routeProvider.when("/login", {templateUrl: 'app/login/login-tpl.html'});
         $routeProvider.when("/users/:userId/task", {templateUrl: 'app/bpm/task/tasks.tpl.html', controller: 'TaskCtrl'});
