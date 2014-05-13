06308312a0022df54e019c43cac3e74770f3ba79
Colin PUY <colin.puy@bonitasoft.com>
2014-05-13 11:27:00 +0200

    change route for user process

--- hackathon-1-angular/src/app/route.js
+++ hackathon-1-angular/src/app/route.js
@@ -7,8 +7,7 @@ angular.module('routes', [])
         $routeProvider.when("/teammanager/users", {templateUrl: 'app/teammanager/users.tpl.html', controller: 'listUsersCtrl'});
         $routeProvider.when("/teammanager/users/:id", {templateUrl: 'app/teammanager/user.tpl.html', controller: 'editUserCtrl'});
         $routeProvider.when("/teammanager/users/:id/processes", {templateUrl: 'app/bpm/processes/processes-tpl.html', controller: 'ProcessesCtrl'});
-
-        $routeProvider.when("/bpm/process/:processId/:userId", {templateUrl: 'app/bpm/processes/start-process-tpl.html', controller: 'StartProcessCtrl'});
+        $routeProvider.when("/teammanager/users/:userId/processes/:processId", {templateUrl: 'app/bpm/processes/start-process-tpl.html', controller: 'StartProcessCtrl'});
 
 
         $routeProvider.when("/login", {templateUrl: 'app/login/login-tpl.html'});
