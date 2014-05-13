d5bd1608c312af70c8f2a87431d4b33166302f75
Colin PUY <colin.puy@bonitasoft.com>
2014-05-13 14:50:23 +0200

    begin task three panel

--- hackathon-1-angular/src/app/bpm/task/tasks.js
+++ hackathon-1-angular/src/app/bpm/task/tasks.js
@@ -1,12 +1,38 @@
-angular.module('tasks', ['resources.task'])
-    .controller('TaskCtrl', function($scope, Tasks, $route, Users) {
+angular.module('tasks', ['resources.task', 'ui.router'])
+    .config(function($stateProvider){
+
+        $stateProvider.state('showTaskDetails', {
+            views: {
+                "taskquickdetails": {
+                    templateUrl: "app/bpm/task/task-qkdetails.tpl.html",
+                    controller: function($scope){
+                        $scope.task = $scope.$parent.tasks[$scope.$parent.selectedIndex];
+                        $scope.currentUserId = $scope.$parent.currentUserId;
+                    }
+                }
+
+            }
+        });
+        $stateProvider.state('startTaskFor', {
+            views: {
+                "taskquickdetails": {
+                    templateUrl: "app/bpm/task/task-do.tpl.html",
+                    controller:'StartProcessCtrl'
+                }
+
+            }
+        });
+    })
+    .controller('TaskCtrl', function($scope, Tasks, $route, Users, $state) {
         Tasks.query({f: ['state=ready', "user_id="+$route.current.params.userId]}, callback);
 
         function userCallback(data) {
             $scope.user =  data;
         }
 
-        Users.getById($route.current.params.userId, userCallback);
+        $scope.currentUserId = $route.current.params.userId;
+
+        Users.getById($scope.currentUserId, userCallback);
 
 
 
@@ -33,4 +59,9 @@ angular.module('tasks', ['resources.task'])
             $scope.src = '/bonita/portal/homepage?ui=form&locale=fr#form=Buy+a+mini+extended--6.2--Model+choice$entry&task='+ task.id +'&mode=form&assignTask=true';
         };
 
+        $scope.selectedIndex = -1;
+        $scope.details = function(task, index) {
+            $scope.selectedIndex = index;
+            $state.go('showTaskDetails');
+        };
     });
