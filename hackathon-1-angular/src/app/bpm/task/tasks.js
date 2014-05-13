42e749d0adf9fe94873d07ea26c4b31c5c4bae2d
julien-reboul <julien.reboul@bonitasoft.com>
2014-05-13 10:02:59 +0200

    make tasks search filter works

--- hackathon-1-angular/src/app/bpm/task/tasks.js
+++ hackathon-1-angular/src/app/bpm/task/tasks.js
@@ -1,6 +1,6 @@
 angular.module('tasks', ['resources.task'])
     .controller('TaskCtrl', function(Tasks, $route) {
-        Tasks.query({f: {f: 'state=ready'},f: {f: 'user_id=' + $route.current.params.userId}});
+        Tasks.query({f: ['state=ready', "user_id="+$route.current.params.userId]});
 
         var self = this;
         function callback(data) {
