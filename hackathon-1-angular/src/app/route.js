c1d3458d2755080923185c6104690a4ae6a77924
Colin PUY <colin.puy@bonitasoft.com>
2014-05-13 11:30:28 +0200

    change route for user tasks

--- hackathon-1-angular/src/app/route.js
+++ hackathon-1-angular/src/app/route.js
@@ -8,10 +8,9 @@ angular.module('routes', [])
         $routeProvider.when("/teammanager/users/:id", {templateUrl: 'app/teammanager/user.tpl.html', controller: 'editUserCtrl'});
         $routeProvider.when("/teammanager/users/:id/processes", {templateUrl: 'app/bpm/processes/processes-tpl.html', controller: 'ProcessesCtrl'});
         $routeProvider.when("/teammanager/users/:userId/processes/:processId", {templateUrl: 'app/bpm/processes/start-process-tpl.html', controller: 'StartProcessCtrl'});
-
+        $routeProvider.when("/teammanager/users/:userId/tasks", {templateUrl: 'app/bpm/task/tasks.tpl.html', controller: 'TaskCtrl'});
 
         $routeProvider.when("/login", {templateUrl: 'app/login/login-tpl.html'});
-        $routeProvider.when("/users/:userId/task", {templateUrl: 'app/bpm/task/tasks.tpl.html', controller: 'TaskCtrl'});
 
         $routeProvider.otherwise({redirectTo: '/teammanager/users'});
     }]);
