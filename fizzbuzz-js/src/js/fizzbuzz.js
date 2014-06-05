4c22df60d30c3e719b3e759796b4fac32abae46a
Vincent <vincent.elcrin@gmail.com>
2014-06-05 13:31:38 +0200

    Detect 3 to be fizz

--- fizzbuzz-js/src/js/fizzbuzz.js
+++ fizzbuzz-js/src/js/fizzbuzz.js
@@ -1,36 +1,10 @@
-var fizzbuzzer = function () {
+var bonitasoft = {};
 
-    var FIZZ = 3;
-    var BUZZ = 7
 
-    function should(fizzOrBuzz, number) {
-        return (contains(number, fizzOrBuzz.toString()) || isMultipleOf(number, fizzOrBuzz))
-    };
-
-    function contains(number, contained) {
-        return number.toString().indexOf(contained) >= 0;
+bonitasoft.fizzbuzz = function(number) {
+    var result = "" + number;
+    if(result.indexOf("3") > 0) {
+        result = "fizz";
     }
-
-    function isMultipleOf(number, diviser) {
-        return number % diviser == 0;
-    }
-
-    return {
-
-        fizzbuzz: function (number) {
-
-            if (should(FIZZ, number) && should(BUZZ, number)) {
-                return "FizzBuzz"
-            }
-
-            if (should(FIZZ, number)) {
-                return 'Fizz';
-            }
-
-            if (should(BUZZ, number)) {
-                return 'Buzz';
-            }
-            return number.toString();
-        }
-    }
-}();
+    return result;
+};
--- fizzbuzz-js/src/spec/fizzbuzz.spec.js
+++ fizzbuzz-js/src/spec/fizzbuzz.spec.js
@@ -2,36 +2,14 @@
 'use strict';
 
 (function () {
-    describe('Fizzbuzzer', function () {
+    describe('Fizzbuzz', function () {
 
-        it('should return the given number in string', function () {
-            var number = fizzbuzzer.fizzbuzz(4);
-
-            expect(number).toBe('4');
+        it('should return a number as a string', function () {
+            expect(bonitasoft.fizzbuzz(2)).toBe("2");
         });
 
-       it('should return Fizz when the given number contains a 3', function () {
-            var number = fizzbuzzer.fizzbuzz(123);
-
-            expect(number).toBe('Fizz');
-        });
-
-        it('should return Fizz when the given number is a multiple of 3', function () {
-            var number = fizzbuzzer.fizzbuzz(12);
-
-            expect(number).toBe('Fizz');
-        });
-
-        it('should return Buzz when the given number contains a 7', function () {
-            var number = fizzbuzzer.fizzbuzz(778);
-
-            expect(number).toBe('Buzz');
-        });
-
-        it('should return FizzBuzz when the given number is Fizz and Buzz', function () {
-            var number = fizzbuzzer.fizzbuzz(37);
-
-            expect(number).toBe('FizzBuzz');
+        it('should return fizz when number contains 3', function () {
+            expect(bonitasoft.fizzbuzz(23)).toBe("fizz");
         });
     });
 })();
