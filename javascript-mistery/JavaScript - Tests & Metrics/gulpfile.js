50192ae94a1a49e9d19c312bc0b2686c48a0dfd5
julien-reboul <julien.reboul@bonitasoft.com>
2015-06-22 18:18:05 +0200

    initial commit

---
+++ javascript-mistery/JavaScript - Tests & Metrics/gulpfile.js
@@ -0,0 +1,31 @@
+var gulp = require('gulp'),
+  	connect = require('gulp-connect'),
+	open = require("gulp-open");
+
+gulp.task('connect', function () {
+  connect.server({
+    root: 'src',
+    port: 3000,
+    livereload: true
+  });
+});
+
+gulp.task('html', function () {
+  gulp.src('./src/*.html')
+    .pipe(connect.reload());
+});
+
+gulp.task('watch', function () {
+  gulp.watch(['./src/*.html'], ['html']);
+});
+
+gulp.task('launch', function(){
+	var options = {
+    	url: 'http://localhost:3000',
+    	app: 'google-chrome-stable'
+  	};
+  gulp.src('./src/index.html')
+  .pipe(open('',options));
+});
+
+gulp.task('default', ['connect', 'watch', 'launch']);
